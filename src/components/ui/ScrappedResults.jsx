3bfbe3e77a728f425c92629786751e59a6f5b711
yamil351 <leokasper@live.com.ar>
2023-09-02 19:47:48 -0300

    results cards

--- src/components/ui/ScrappedResults.jsx
+++ src/components/ui/ScrappedResults.jsx
@@ -1,6 +1,18 @@
+import { styled } from 'styled-components';
+
+const Container = styled.article``;
+const Wrapper = styled.div``;
 
 const ScrappedResults = () => {
-  return <div>results</div>;
+  return (
+    <Container>
+      <Wrapper>
+        lajdlajdlajdlkajdlakjdlajd lkajlajd aldjaldjlkadjla alkdjalkdja
+        asdljaldjasldjalkdjalkdjaldja aldjaldj adj aldjaldjad l jaldjas ljdal
+        jadl aldjaldj
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default ScrappedResults;
