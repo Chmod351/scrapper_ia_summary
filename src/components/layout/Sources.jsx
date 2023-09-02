27322bb6fd4898320670df4ee9c017d68a05100a
yamil351 <leokasper@live.com.ar>
2023-09-02 13:47:06 -0300

    hardcoded color chnge

--- src/components/layout/Sources.jsx
+++ src/components/layout/Sources.jsx
@@ -3,11 +3,12 @@ import { main } from '../../config/theme.js';
 import { useState } from 'react';
 import { mobile } from '../../config/responsive.js';
 
+
 const Wrapper = styled.div`
   display: flex;
   width: 15%;
   background-color: ${main.primary};
-  color: white;
+  color: ${main.primaryBg};
   height: 2.5rem;
   margin: auto;
   text-align: center;
